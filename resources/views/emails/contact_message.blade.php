2af5a9a52a7276650e4bcb920844b8d40b7e12e6
abubdcalling <abubdcalling@gmail.com>
2025-04-22 11:50:32 +0600

    update

--- resources/views/emails/contact_message.blade.php
+++ resources/views/emails/contact_message.blade.php
@@ -1,6 +1,7 @@
 <h2>New Contact Message Received</h2>
 
-<p><strong>Name:</strong> {{ $contact->name }}</p>
-<p><strong>Email:</strong> {{ $contact->email }}</p>
-<p><strong>Phone Number:</strong> {{ $contact->number }}</p>
-<p><strong>Message:</strong> {{ $contact->message }}</p>
+<p><strong>Name:</strong> {{ $contact->first_name }} {{ $contact->last_name }}</p>
+<p><strong>Email:</strong> {{ $contact->email_address }} </p>
+<p><strong>Phone Number:</strong> {{ $contact->phone_number }}</p>
+<p><strong>Company:</strong> {{ $contact->company_name }}</p>
+<p><strong>Message:</strong> {{ $contact->comments }}</p>
